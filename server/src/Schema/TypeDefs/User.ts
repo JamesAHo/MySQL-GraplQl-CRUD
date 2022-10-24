144a2b5c3eed0d13df08b24535da1cc1df8660e9
James Anh <james.anh184@gmail.com>
2022-10-23 19:06:02 -0700

    initialize graphql client

---
+++ server/src/Schema/TypeDefs/User.ts
@@ -0,0 +1,12 @@
+// Define schema
+import {GraphQLObjectType, GraphQLID, GraphQLString} from 'graphql';
+
+export const UserType = new GraphQLObjectType({
+    name:"User",
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        username: {type: GraphQLString},
+        password: {type: GraphQLString}
+    }),
+});
---
+++ server/src/Schema/index.ts
@@ -0,0 +1,18 @@
+import {GraphQLSchema, GraphQLObjectType} from 'graphql'
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: {}
+})
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {}
+})
+
+
+
+
+export const schema = new GraphQLSchema({
+    query:RootQuery, 
+    mutation: Mutation,
+})
